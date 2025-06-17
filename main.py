cc3bd2b12fdb52bfa697a089cef7ccff7efa9ef4
Ivan Monyakov <ivanmoniakov360@gmail.com>
2025-06-18 01:00:16 +0300

    add class deploy

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ def add(a: int, b: int) -> int:
     return a + b
 
 print(add(first_num, second_num))
+
+class deploy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    @property
+    def coord(self):
+        return self.x, self.y
+    
+d = deploy(20, 40)
+
+print(d.coord)
+
